359ad173ff97eb766de0c4cd1f0912256159da21
Josh Derocher-Vlk <josh.derocher-vlk@simplisafe.com>
2020-06-30 12:36:10 -0400

    testing PR

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
-import React from "react"
 import { Link } from "gatsby"
+import React from "react"
 
-import Layout from "../components/layout"
 import Image from "../components/image"
+import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
+    <h2>A Change!</h2>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
