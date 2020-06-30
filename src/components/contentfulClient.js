773c32c294128dce991c05d18bf6336c191d83a0
Josh Derocher-Vlk <josh.derocher-vlk@simplisafe.com>
2020-06-30 14:42:20 -0400

    fix build

--- src/components/contentfulClient.js
+++ src/components/contentfulClient.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-unused-vars */
 import Maybe from 'crocks/Maybe'
+import fetch from 'node-fetch'
+
 const contentful = require('contentful')
 const { encaseP, map, fork, reject, resolve, chain } = require('fluture')
 
 export const spaceId = '0wm9qd6toywt'
 export const contentDeliveryToken = 'YeZK36wA90IrT4WLW1a4Ap3UAWffbyEMEaX_552hJdg'
 export const contentPreviewToken = '-luKhnJQT_43JPGLHlbwWDm7AH-hAHL2dA90EY9JYw4'
+
 
 const parseBody = Maybe
 
@@ -15,7 +18,8 @@ const verifyResponse = res => res && res.statusCode === 200
 
 export const client = contentful.createClient({
   space: spaceId,
-  accessToken: contentDeliveryToken
+  accessToken: contentDeliveryToken,
+  fetch
 })
 
 const getEntryFuture = encaseP(entry => client.getEntry(entry))
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,27 +2,16 @@ import { Container } from '@material-ui/core'
 import { Link } from "gatsby"
 import React from "react"
 
-import Banner from '../components/Banner'
 import ContentfulBanner from '../components/ContentfulBanner'
 import Image from "../components/image"
 import Layout from "../components/layout"
-import ProductList from '../components/ProductsList'
 import SEO from "../components/seo"
-import { useLanguage } from '../hooks'
 
 const IndexPage = () => {
-   const language = useLanguage()
   return (
   <Layout>
     <SEO title="Home" />
-    
-  return <>
-    <Banner language={language}/>
     <ContentfulBanner />
-    <Container>
-      <ProductList language={language}/>
-    </Container>
-  </>
   </Layout>
   )
 }
