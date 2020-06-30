5eb0f777af986e5eb964151c2393c560d549d18e
Josh Derocher-Vlk <josh.derocher-vlk@simplisafe.com>
2020-06-30 12:39:34 -0400

    another change!

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <h2>A Change!</h2>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Gatsby site!</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
